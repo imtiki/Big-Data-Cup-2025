4dbbfe4a0c545f77c22d1ae6bcfdc6e46413f06d
imti <imtiazkidwai@gmail.com>
2025-01-05 21:33:41 -0500

    Created tracking data tables in PostgreSQL

---
+++ docs/creating tables.sql
@@ -0,0 +1,36 @@
+CREATE TABLE ba_tracking(
+"Image Id" VARCHAR(100),
+"Period" INT,
+"Game Clock" VARCHAR(100),
+"Player or Puck" VARCHAR(100),
+"Team" VARCHAR(100),
+"Player Id" INT,
+"Rink Location X (Feet)" DECIMAL, 
+"Rink Location Y (Feet)" DECIMAL,
+"Rink Location Z (Feet)" DECIMAL,
+"Goal Score" VARCHAR(100)
+);
+CREATE TABLE dc_tracking(
+"Image Id" VARCHAR(100),
+"Period" INT,
+"Game Clock" VARCHAR(100),
+"Player or Puck" VARCHAR(100),
+"Team" VARCHAR(100),
+"Player Id" INT,
+"Rink Location X (Feet)" DECIMAL, 
+"Rink Location Y (Feet)" DECIMAL,
+"Rink Location Z (Feet)" DECIMAL,
+"Goal Score" VARCHAR(100)
+);
+CREATE TABLE fe_tracking(
+"Image Id" VARCHAR(100),
+"Period" INT,
+"Game Clock" VARCHAR(100),
+"Player or Puck" VARCHAR(100),
+"Team" VARCHAR(100),
+"Player Id" INT,
+"Rink Location X (Feet)" DECIMAL, 
+"Rink Location Y (Feet)" DECIMAL,
+"Rink Location Z (Feet)" DECIMAL,
+"Goal Score" VARCHAR(100)
+);
